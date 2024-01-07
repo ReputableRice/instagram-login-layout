97867c8f1425589fa046d0aa3bf02e1ee9cb1168
Skailodon <jackiehtruong@outlook.com>
2024-01-07 11:20:52 -0800

    more query edits

--- components/GetApp/index.js
+++ components/GetApp/index.js
@@ -9,8 +9,9 @@ export default function App() {
     <div className={styles.apptext}> Get the app.</div>
 
     <div className={styles.images}>
-        <Image className={styles.microsoft} src={'/images/apple.png'} width={126} height={45} alt="Download on the App Store"></Image>
-        <Image className={styles.googlePlay} src={'/images/Google_Play_Store_badge_EN.svg.png'} width={151} height={45} alt="Get it on Google Play"></Image>
+        <Image className={styles.googlePlay} src={'/images/Google_Play_Store_badge_EN.svg.png'} width={151} height={45} alt="Get it on Google Play"></Image>        
+        <Image className={styles.microsoft} src={'/images/Microsoft.png'} width={126} height={45} alt="Get is on Microsoft"></Image>
+
     </div>
 </div>
 
